b46c66ff978ee1cf7d61978991945804d115a651
Nick Dellar <ndelfern@live.ca>
2022-09-13 03:11:31 +0000

    breedFetcher implement w/ 2 mocha chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,22 @@
 const request = require('request');
-const breed = process.argv[2];
 
-if (breed === undefined) {
-  console.log(`No meows found`);
-  return;
-}
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-    return;
+const fetchBreedDescription = function(breedName, callback) {
+  if (breedName === undefined) {
+    return `No cats found`;
   }
-  const data = JSON.parse(body);
-  console.log(data[0].description);
-});
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback('breed not found', null);
+      return;
+    }
+    callback(null, data[0].description);
+    return;
+  });
+};
+
+module.exports = { fetchBreedDescription };
